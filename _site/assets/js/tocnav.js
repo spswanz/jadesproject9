d664e1688238ffb5aa79c8da68b01da29198c5a7
sarahrider <85252768+sarahrider@users.noreply.github.com>
2023-08-17 10:09:41 -0500

    Fixing CSS

---
+++ _site/assets/js/tocnav.js
@@ -0,0 +1,8 @@
+{% for item in jadesproject9._data.toc %}
+    <h3>{{ item.title }}</h3>
+      <ul>
+        {% for entry in item.subfolderitems %}
+          <li><a href="{{ entry.url }}">{{ entry.page }}</a></li>
+        {% endfor %}
+      </ul>
+  {% endfor %}
